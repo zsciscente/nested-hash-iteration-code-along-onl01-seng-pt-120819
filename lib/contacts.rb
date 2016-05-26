f91037b11494205cea2cdfe91d65178dfe3669a3
Avidor Turkewitz <aviturkewitz@gmail.com>
2016-05-26 15:34:25 -0400

    Update solution to use ice_cream instead of icecream

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -4,20 +4,20 @@ require 'pry'
   # contacts = {
   #   "Jon Snow" => {
   #     name: "Jon",
-  #     email: "jon_snow@thewall.we", 
-  #     favorite_icecream_flavors: ["chocolate", "vanilla"]
+  #     email: "jon_snow@thewall.we",
+  #     favorite_ice_cream_flavors: ["chocolate", "vanilla"]
   #   },
   #   "Freddy Mercury" => {
   #     name: "Freddy",
   #     email: "freddy@mercury.com",
-  #     favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
+  #     favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
   #   }
   # }
 
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     contact_details_hash.each do |attribute, data|
-      if attribute == :favorite_icecream_flavors
+      if attribute == :favorite_ice_cream_flavors
         data.delete_if {|ice_cream| ice_cream == "strawberry"}
       end
     end
