38804da0597963de25ddffcf82a74dc61c13804a
Kellye Greene <kellyecodes@gmail.com>
2019-06-04 11:59:08 -0400

    Updates solution file

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -16,9 +16,11 @@ require 'pry'
 
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
-    contact_details_hash.each do |attribute, data|
-      if attribute == :favorite_ice_cream_flavors
-        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
+        if attribute == :favorite_ice_cream_flavors
+          data.delete_if {|ice_cream| ice_cream == "strawberry"}
+        end
       end
     end
   end
