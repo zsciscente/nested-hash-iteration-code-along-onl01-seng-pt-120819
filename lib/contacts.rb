d0d6a04d4c36bd919b48a83418690dc80ca4344d
Zachary Sciscente <zsciscente@gmail.com>
2019-12-22 23:58:26 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,18 +14,7 @@ require 'pry'
   #   }
   # }
   
-  contacts = {
-  "Jon Snow" => {
-    name: "Jon",
-    email: "jon_snow@thewall.we",
-    favorite_ice_cream_flavors: ["chocolate", "vanilla"]
-  },
-  "Freddy Mercury" => {
-    name: "Freddy",
-    email: "freddy@mercury.com",
-    favorite_ice_cream_flavors: ["strawberry", "cookie dough", "mint chip"]
-  }
-}
+
   
 def remove_strawberry(contacts)
   contacts.each do |person, contacts_details_hash|
