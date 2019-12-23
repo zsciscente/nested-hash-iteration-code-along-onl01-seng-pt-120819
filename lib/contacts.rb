4e224c716f29113711dc55f292e3e7f3bc3098c6
Zachary Sciscente <zsciscente@gmail.com>
2019-12-23 00:10:47 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -14,6 +14,16 @@ require 'pry'
   #   }
   # }
   
-def remove_strawberry(contacts)
 
+  
+def remove_strawberry(contacts)
+ contacts.each do |person, contact_details_hash|
+  if person == "Freddy Mercury"
+    contact_details_hash.each do |attribute, data|
+      if attribute == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+      end
+    end
+  end
+end
 end
