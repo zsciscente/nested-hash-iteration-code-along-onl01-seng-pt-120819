3c14ffd8eb3118cbb2d5af77ddb11fef2137b476
Zachary Sciscente <zsciscente@gmail.com>
2019-12-23 00:04:33 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -18,6 +18,9 @@ require 'pry'
   
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
+    if person == "Freddy Mercury"
+      contact_details_hash.each do |attribute, data|
     binding.pry
   end
+end
 end
